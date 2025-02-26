713fc3083dcb87f3c369a51776367f77e4f3ba42
ABDULMALEK  AL-SHUGAA <33663456+AbdulmalekAlshugaa@users.noreply.github.com>
2025-02-26 19:34:13 +0800

    chore: display error if there is any

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "expo-router";
-import { NetworkStatusToast } from "netly-rn-expo";
+import { NetworkStatusToast, useNetworkStatus } from "netly-rn-expo";
 import { Text, View } from "react-native";
 
 export default function RootLayout() {
   return (
     <View style={{ flex: 1, justifyContent:'center', alignItems:'center' }}>
-      <NetworkStatusToast toastHeight={80} />
+      <NetworkStatusToast 
+      
+      toastHeight={100} />
       <Text>Welcome to my app!</Text>
     </View>
   );
