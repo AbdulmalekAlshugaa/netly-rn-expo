9f2f6783c301d8e49b9108b1eefe7570531a4d16
ABDULMALEK  AL-SHUGAA <33663456+AbdulmalekAlshugaa@users.noreply.github.com>
2025-02-26 17:31:22 +0800

    chore: update rootLayout

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, justifyContent:'center', alignItems:'center' }}>
       <NetworkStatusToast toastHeight={80} />
       <Text>Welcome to my app!</Text>
     </View>
