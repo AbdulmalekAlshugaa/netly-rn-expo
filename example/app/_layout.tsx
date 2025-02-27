6a2f8190f4955b81ac7a74dc9a75a81010d79c44
ABDULMALEK  AL-SHUGAA <33663456+AbdulmalekAlshugaa@users.noreply.github.com>
2025-02-27 12:59:44 +0800

    chore: bump lib version

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -1,13 +1,18 @@
 import { Stack } from "expo-router";
 import { NetworkStatusToast, useNetworkStatus } from "netly-rn-expo";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function RootLayout() {
+  const networkStatus = useNetworkStatus();
+  useEffect(()=>{
+    console.log('networkStatus', networkStatus);
+  },[])
   return (
     <View style={{ flex: 1, justifyContent:'center', alignItems:'center' }}>
       <NetworkStatusToast 
       
-      toastHeight={100} />
+      />
       <Text>Welcome to my app!</Text>
     </View>
   );
